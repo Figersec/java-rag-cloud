f47b00491f354c938da08064ef2567a6ae017dd7
yangsong <178014451@qq.com>
2023-06-28 14:45:48 +0800

    增加枚举配置

---
+++ java-chat-cloud-data/src/main/java/com/kailin/dao/enums/EnumTypeHandler.java
@@ -0,0 +1,67 @@
+
+package com.kailin.dao.enums;
+
+import com.baomidou.mybatisplus.annotation.IEnum;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author 杨松
+ * @param <E>
+ */
+@MappedTypes({IEnum.class})
+public class EnumTypeHandler<E extends Enum<E>> extends BaseTypeHandler<E> {
+
+    private final Class<E> type;
+
+    public EnumTypeHandler(Class<E> type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Type argument cannot be null");
+        }
+        this.type = type;
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
+        if (jdbcType == null) {
+            ps.setString(i, parameter.name());
+        } else {
+            ps.setObject(i, parameter.name(), jdbcType.TYPE_CODE);
+        }
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String s = rs.getString(columnName);
+        return StringUtils.isBlank(s) ? null : Enum.valueOf(type, s);
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String s = rs.getString(columnIndex);
+        return StringUtils.isBlank(s) ? null : Enum.valueOf(type, s);
+    }
+
+    @Override
+    public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String s = cs.getString(columnIndex);
+        if (StringUtils.isBlank(s)) {
+            return null;
+        }
+        E e;
+        try {
+            e = Enum.valueOf(type, s);
+        } catch (Exception ex) {
+            e = null;
+        }
+        return e;
+    }
+
+}
---
+++ java-chat-cloud-data/src/main/java/com/kailin/dao/util/ReflectUtil.java
@@ -0,0 +1,36 @@
+package com.kailin.dao.util;
+
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+/**
+ * 反射工具类
+ *
+ * @author 杨松
+ */
+public class ReflectUtil {
+
+    /**
+     * 分离最后一个代理的目标对象
+     *
+     * @param invocation
+     * @return
+     */
+    public static MetaObject getRealTarget(Invocation invocation) {
+        MetaObject metaStatementHandler = SystemMetaObject.forObject(invocation.getTarget());
+
+        while (metaStatementHandler.hasGetter("h")) {
+            Object object = metaStatementHandler.getValue("h");
+            metaStatementHandler = SystemMetaObject.forObject(object);
+        }
+
+        while (metaStatementHandler.hasGetter("target")) {
+            Object object = metaStatementHandler.getValue("target");
+            metaStatementHandler = SystemMetaObject.forObject(object);
+        }
+
+        return metaStatementHandler;
+    }
+
+}
